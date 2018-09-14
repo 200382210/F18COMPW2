83ba64bdffdecf14cb2ded96ff92d7077bcdedf2
Dhruv Patel <200382210@student.georgianc.on.ca>
2018-09-14 10:02:37 -0400

    Initial commit

---
+++ src/Models/DBConnect.java
@@ -0,0 +1,49 @@
+package Models;
+
+
+import java.sql.*;
+import java.util.ArrayList;
+
+public class DBConnect {
+    private static String user="root";
+    private static String password="";
+
+    public static ArrayList<String> getPhoneManufacturers() throws SQLException {
+        ArrayList<String> manufacturers = new ArrayList<>();
+        Connection conn=null;
+        Statement statement =null;
+        ResultSet resultSet=null;
+
+        try{
+            //1.connect to the database
+            conn= DriverManager.getConnection("jdbc:mysql://localhost:3306/phones?useSSL=false"
+                    ,user,password);
+
+            //2.create a statement object
+            statement = conn.createStatement();
+
+            //3.create a sql query
+            resultSet=statement.executeQuery("SELECT*FROM manufacturers");
+
+            //4.loop over the results and add it to the array list
+            while(resultSet.next()){
+                manufacturers.add(resultSet.getString("manufacturer"));
+            }
+        }
+        catch(SQLException e){
+            System.err.println(e);
+        }
+        finally {
+            if(conn!=null)
+                conn.close();
+            if(statement!=null)
+                statement.close();
+            if(resultSet!=null)
+                resultSet.close();
+        }
+        return manufacturers;
+
+    }
+
+
+}
